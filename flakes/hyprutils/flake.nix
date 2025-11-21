11ce6e09518c56d3855345074c3000640d43ca25
Vaxry <vaxry@vaxry.net>
2025-11-21 20:42:32 +0000

    stuf

--- flakes/hyprutils/flake.nix
+++ flakes/hyprutils/flake.nix
@@ -1,35 +1,7 @@
 {
   description = "Wrapper flake for stable hyprutils";
 
-  inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-    hyprutils.url = "github:hyprwm/hyprutils/v0.10.2";
-  };
+  inputs.hyprutils.url = "github:hyprwm/hyprutils/v0.10.2";
 
-  outputs = { self, nixpkgs, hyprutils, ... }:
-  let
-    systems = [ "x86_64-linux" "aarch64-linux" ];
-    forAllSystems = f:
-      builtins.listToAttrs (map (system: {
-        name = system;
-        value = f system;
-      }) systems);
-  in
-  {
-    overlays.default = final: prev: {
-      hyprutils = final.callPackage ./nix/default.nix { };
-    };
-
-    packages = forAllSystems (system:
-      let
-        defaultPkg = import nixpkgs {
-          inherit system;
-          overlays = [ self.overlays.default ];
-        };
-      in
-      {
-        hyprutils = defaultPkg;
-        default = defaultPkg;
-      });
-  };
+  outputs = { self, hyprutils, ... }: hyprutils;
 }
