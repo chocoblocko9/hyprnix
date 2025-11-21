9f71a3d98b169791d4ff2733c088ce561cdc751b
Vaxry <vaxry@vaxry.net>
2025-11-21 20:19:54 +0000

    stuf

--- flake.nix
+++ flake.nix
@@ -5,9 +5,10 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
 
     hyprland.url = "path:./flakes/hyprland";
+    hyprutils.url = "path:./flakes/hyprutils";
   };
 
-  outputs = inputs @ { self, nixpkgs, hyprland, ... }: let
+  outputs = inputs @ { self, nixpkgs, hyprland, hyprutils, ... }: let
       systems = [ "x86_64-linux" "aarch64-linux" ];
       forAllSystems = f:
         nixpkgs.lib.genAttrs systems (system:
@@ -19,6 +20,7 @@
       packages = forAllSystems ({ pkgs, system }:
         {
           hyprland = hyprland.packages.${system}.default;
+          hyprutils = hyprutils.packages.${system}.default;
         });
     };
 }
