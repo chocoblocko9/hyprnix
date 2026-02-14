704ab62552d36f011e99b5fba3381ecf4b45dc6b
chocoblocko9 <91826251+chocoblocko9@users.noreply.github.com>
2026-02-14 21:03:01 +0000

    Update hyprland input to use main branch because 0.53.3 doesn't build

--- flake.nix
+++ flake.nix
@@ -38,7 +38,7 @@
     };
 
     hyprland = {
-      url = "github:hyprwm/hyprland/v0.53.3";
+      url = "github:hyprwm/hyprland";
       inputs.nixpkgs.follows = "nixpkgs";
       inputs.systems.follows = "systems";
       inputs.aquamarine.follows = "aquamarine";
